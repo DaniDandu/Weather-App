aedd658c1028d6f67db6d403d26ed7b1d6255212
DaniDandu <danidandu19@gmail.com>
2025-05-07 22:30:51 +0300

    getting data from API

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Selectare elemente
+const apiKey = "06efa3513dd9863039b20d7222dee51d";
+
+document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.querySelector('.menu-icon');
     const closeMenu = document.querySelector('.close-menu');
     const menuSidebar = document.querySelector('.menu-sidebar');
@@ -7,9 +8,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputContainer = document.querySelector('.input-container');
     const headerTop = document.querySelector('.header-top');
     const searchBtn = document.querySelector('.search-btn');
+    const cityInput = document.querySelector('.city-input');
+    const weatherInfoSection = document.querySelector('.weather-info')
+    const cityTxt = document.querySelector('.country-txt');
+    const tempTxt = document.querySelector('.temp-txt');
+    const conditionTxt = document.querySelector('.condition-txt');
+    const feelsTemp = document.querySelector('.feels-value-txt');
+    const cloudsValueTxt = document.querySelector('.clouds-value-txt');
+    const humidityValueTxt = document.querySelector('.humidity-value-txt');
+    const windValueTxt = document.querySelector('.wind-value-txt');
+    const weatherSummaryImg = document.querySelector('.weather-summary-img');
+    const currentDateTxt = document.querySelector('.current-date-txt')
+
 
     // Funcționalitate meniu
-    if(menuIcon) {
+    if (menuIcon) {
         menuIcon.addEventListener('click', (e) => {
             e.stopPropagation();
             menuSidebar.classList.add('active');
@@ -17,14 +30,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Închidere meniu
-    if(closeMenu) {
+    if (closeMenu) {
         closeMenu.addEventListener('click', () => {
             menuSidebar.classList.remove('active');
         });
     }
 
     // Funcționalitate search
-    if(searchIcon) {
+    if (searchIcon) {
         searchIcon.addEventListener('click', (e) => {
             e.stopPropagation();
             inputContainer.classList.add('active');
@@ -36,12 +49,12 @@ document.addEventListener('DOMContentLoaded', function() {
     // Închidere search la click în afară
     document.addEventListener('click', (e) => {
         // Închide meniul
-        if(!menuSidebar.contains(e.target) && !menuIcon.contains(e.target)) {
+        if (!menuSidebar.contains(e.target) && !menuIcon.contains(e.target)) {
             menuSidebar.classList.remove('active');
         }
 
         // Închide search bar
-        if(!inputContainer.contains(e.target) && !searchIcon.contains(e.target)) {
+        if (!inputContainer.contains(e.target) && !searchIcon.contains(e.target)) {
             inputContainer.classList.remove('active');
             headerTop.style.opacity = '1';
             headerTop.style.pointerEvents = 'all';
@@ -49,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Închidere search la submit
-    if(searchBtn) {
+    if (searchBtn) {
         searchBtn.addEventListener('click', (e) => {
             e.preventDefault();
             inputContainer.classList.remove('active');
@@ -66,13 +79,67 @@ document.addEventListener('DOMContentLoaded', function() {
         headerTop.style.pointerEvents = 'all';
     });
 
-    // Opțional: Închidere la apăsare Escape
-    document.addEventListener('keydown', (e) => {
-        if(e.key === 'Escape') {
-            menuSidebar.classList.remove('active');
-            inputContainer.classList.remove('active');
-            headerTop.style.opacity = '1';
-            headerTop.style.pointerEvents = 'all';
+    searchBtn.addEventListener('click', () => {
+        if (cityInput.value.trim() != '') {
+            updateWeatherInfo(cityInput.value)
+            cityInput.value = ''
         }
-    });
+    })
+
+    cityInput.addEventListener('keydown', (event) => {
+        if (event.key == 'Enter' &&
+            cityInput.value.trim() != ''
+        ) {
+            updateWeatherInfo(cityInput.value)
+            cityInput.value = ''
+        }
+    })
+
+
+    async function getFetchData(endPoint, city) {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
+    
+        const response = await fetch(apiUrl);
+    
+        return response.json();
+    }
+
+    function getWeatherIcon(id) {
+        if (id <= 232) return 'thunderstorm.svg'
+        if (id <= 321) return 'drizzle.svg'
+        if (id <= 531) return 'rain.svg'
+        if (id <= 622) return 'snow.svg'
+        if (id <= 781) return 'atmosphere.svg'
+        if (id <= 800) return 'clear.svg'
+        else return 'clouds.svg'
+    }
+    
+    async function updateWeatherInfo(city) {
+        const weatherData = await getFetchData('weather', city);
+        console.log(weatherData);
+    
+        const {
+            name: country,
+            main: { temp, temp_max, temp_min, feels_like, humidity}, 
+            clouds: { all },
+            weather: [{ id, main }],
+            wind: { speed },
+        } = weatherData;
+    
+        cityTxt.textContent = country
+        tempTxt.textContent = Math.round(temp) + ' °C'
+        conditionTxt.textContent = main
+        // highTemp.textContent = "H: " + Math.round(temp_max) + ' °C'
+        // lowTemp.textContent = "L: " + Math.round(temp_min) + ' °C'
+        feelsTemp.textContent = Math.round(feels_like) + ' °C'
+        humidityValueTxt.textContent = humidity + '%'
+        cloudsValueTxt.textContent = all + '%'
+        windValueTxt.textContent = Math.round(speed * 3.6) + ' km/h'
+
+        weatherSummaryImg.src = `/assets/weather/${getWeatherIcon(id)}`
+    
+    }
+    
+    
 });
+
