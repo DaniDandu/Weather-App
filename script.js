0edbc7cfe46ebac3572d423b084e7e9b54152dc4
DaniDandu <danidandu19@gmail.com>
2025-06-03 20:25:08 +0300

    changed config file to .mjs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { apiKey } from './config.js';
+import { apiKey } from './config.mjs';
 
 document.addEventListener('DOMContentLoaded', function () {
     const splashScreen = document.querySelector('.splash-screen');
